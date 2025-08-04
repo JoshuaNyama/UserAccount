b87805165e4f213557a47aa800874b50c6a95c97
Joshua Nyama <joshuanyama05@gmail.com>
2025-08-04 15:20:01 +0400

    initial commit : added more Application files

---
+++ Account/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        // Shared dictionary to store account info
+        static Dictionary<string, string> accounts = new Dictionary<string, string>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("___Welcome__");
+            Console.WriteLine("(1) Create account" +
+                "\n(2) Sign in");
+
+            Console.Write("Enter yer option: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    CreateAccount();
+                    break;
+                case 2:
+                    SignIn();
+                    break;
+                default:
+                    Console.WriteLine("Invalid input!");
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+        static void CreateAccount()
+        {
+            while (true)
+            {
+                Console.Write("Enter yer name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Create a password: ");
+                string password = Console.ReadLine();
+
+                Console.Write("Confirm yer password: ");
+                string confirmPassword = Console.ReadLine();
+
+                if (password != confirmPassword)
+                {
+                    Console.WriteLine("Passwords do not match. Try again.\n");
+                    continue;
+                }
+
+                Console.Write("Enter yer age (18+): ");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                if (age < 18)
+                {
+                    Console.WriteLine("Yer aren't of age yet.");
+                    break;
+                }
+
+                if (!accounts.ContainsKey(name))
+                {
+                    accounts.Add(name, password);
+                    Console.WriteLine("Account created successfully! Yer can now proceed.\n");
+                }
+                else
+                {
+                    Console.WriteLine("That username already exists. Try a different one.\n");
+                    continue;
+                }
+                break;
+            }
+            Console.WriteLine("Enter PRIE0-1 Navigate --03");
+        }
+
+        static void SignIn()
+        {
+            if (accounts.Count == 0)
+            {
+                // Preloaded accounts if needed
+                accounts.Add("Joshua", "Joshua05");
+                accounts.Add("Josh", "Josh01");
+                accounts.Add("Shadow", "Shadow05");
+                accounts.Add("Nova", "Lord");
+            }
+
+            bool loggedIn = false;
+
+            while (!loggedIn)
+            {
+                Console.WriteLine("___Enter account details___");
+                Console.Write("Enter your username: ");
+                string username = Console.ReadLine();
+
+                Console.Write("Enter your password: ");
+                string password = Console.ReadLine();
+
+                if (accounts.ContainsKey(username) && accounts[username] == password)
+                {
+                    Console.WriteLine("Login successful. Welcome, " + username + "!");
+                    loggedIn = true;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect username or password. Try again.\n");
+                }
+            }
+        }
+    }
+}
---
+++ Account/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Account")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Account")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7b2b65c2-8872-4cc8-92c7-9e0bc7883e4d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
